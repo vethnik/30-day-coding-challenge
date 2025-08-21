5a8ae5d5dea549529cf9593b1c4b6aa5214cfe5b
Melala Venkata Shiva Sai Vethnik <vethnik007@gmail.com>
2025-08-21 22:01:25 +0530

    day 2

---
+++ day2/First Unique Char(lc_387).py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        for i in range(len(s)):
+            if s[i] not in s[:i] and s[i] not in s[i+1:]:
+                return i
+        return -1
